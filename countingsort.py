e7b6f3e5e94b8d425b29a213e76b365b8a44484b
Fábio Melo <fabio-melo@users.noreply.github.com>
2018-08-23 11:33:36 -0300

    Update countingsort.py

--- countingsort.py
+++ countingsort.py
@@ -8,21 +8,23 @@ import sys # para fazer parsing dos argumentos
 
 def csort(lista):
 
-    maiornum = max(lista) #pega o maior numero da lista e usa como tamanho para a lista de contagem
-    
-    count_arr = [0 for x in range(maiornum + 1)] #aloca a lista de contagem
+    menornum, maiornum = min(lista), max(lista)
+
+    OFFSET = 0 if menornum > 0 else abs(menornum)
+
+    count = [0 for x in range(maiornum + OFFSET + 1)] #aloca a lista de contagem
     
     for x in range(0,len(lista)): #monta a lista da contagem de ocorrencias
-        count_arr[lista[x]] = count_arr[lista[x]] + 1
+        count[lista[x]+OFFSET] += 1
 
-    for x in range(1,len(count_arr)): #modifica a lista de forma que os numeros sejam suas posições na lista
-        count_arr[x] = count_arr[x] + count_arr[x-1]
+    for x in range(1,len(count)): #modifica a lista de forma que os numeros sejam suas posições na lista
+        count[x] += count[x-1]
     
     sorted = [0 for x in range(len(lista))] #aloca a lista que irá ser ordenada
     
     for x in range(len(lista)-1,-1,-1): #começa a ordenação
-        sorted[count_arr[lista[x]]-1] = lista[x] #salva o item na sua posição final
-        count_arr[lista[x]] = count_arr[lista[x]] - 1 #decrementa o numero na posição manipulada da lista
+        sorted[count[lista[x]+OFFSET]-1] = lista[x] #salva o item na sua posição final
+        count[lista[x]+OFFSET] -= 1 #decrementa o numero na posição manipulada da lista
         
     return sorted #retorna a lista ordenada
 
